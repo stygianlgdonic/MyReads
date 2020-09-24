b49bef429549bc0539e0d5ed140466313346aae4
stygianlgdonic <stygianlgdonic@gmail.com>
2020-09-24 23:19:26 +0500

    search functionality added

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
+import * as BooksAPI from '../BooksAPI'
+import Book from './Book'
 
 
 export default class Search extends React.Component {
+
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    changeShelf: PropTypes.func.isRequired
+  };
+
+  state = {
+    query: '',
+    showingBooks: []
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query })
+  }
+
   render() {
+
+    const { books, changeShelf } = this.props;
+
+    let showingBooks
+    if (this.state.query) {
+
+      const match = new RegExp(escapeRegExp(this.state.query), 'i')
+
+      BooksAPI.search(this.state.query.trim()).then(
+        showingBooks => {
+          this.setState({ showingBooks })
+        }
+      )
+
+    }
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -19,12 +55,25 @@ export default class Search extends React.Component {
             However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
             you don't find a specific author or title. Every search is limited by search terms.
           */}
-            <input type="text" placeholder="Search by title or author" />
-
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {this.state.showingBooks.map(book => (
+              <Book
+                book={book}
+                books={books}
+                key={book.id}
+                changeShelf={changeShelf}
+              />
+            ))}
+          </ol>
         </div>
       </div>
     )
